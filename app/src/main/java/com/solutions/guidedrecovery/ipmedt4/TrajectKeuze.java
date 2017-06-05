fa55a90a23cb9df8d8faf716ef4a5f1b556d759b
dipak1988 <dipak@silvernet.nl>
2017-06-05 13:29:46 +0200

    eerste commit dip

---
+++ app/src/main/java/com/solutions/guidedrecovery/ipmedt4/TrajectKeuze.java
@@ -0,0 +1,13 @@
+package com.solutions.guidedrecovery.ipmedt4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TrajectKeuze extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_traject_keuze);
+    }
+}
