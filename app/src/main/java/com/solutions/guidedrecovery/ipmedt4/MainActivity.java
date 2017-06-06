b6c13d3cc5bc9fb55dff8f3c57c5354d6e22464d
keanuTA <keanutjin95@hotmail.com>
2017-06-06 09:57:41 +0200

    mijn veranderingen

--- app/src/main/java/com/solutions/guidedrecovery/ipmedt4/MainActivity.java
+++ app/src/main/java/com/solutions/guidedrecovery/ipmedt4/MainActivity.java
@@ -2,32 +2,42 @@ package com.solutions.guidedrecovery.ipmedt4;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.widget.CardView;
 import android.view.View;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView  lijst;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayAdapter<String> mAdapter = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.traject));
+
+        lijst = (ListView) findViewById(R.id.lijst);
+        lijst.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int i, long l) {
+                Intent intent = new Intent(MainActivity.this,TrajectKeuzeActivity.class);
+                intent.putExtra("Trajectpunt",lijst.getItemAtPosition(i).toString());
+            }
+        });
+        lijst.setAdapter(mAdapter);
+
+
    }
 
     public void toTraject(View v) {
         switch (v.getId()) {
             case R.id.card_view_dwars:
-                Intent intent = new Intent(this, TrajectKeuze.class);
+                Intent intent = new Intent(this, TrajectKeuzeActivity.class);
                 startActivity(intent);
                 break;
             case R.id.card_view_meervoudig:
--- app/src/main/java/com/solutions/guidedrecovery/ipmedt4/TrajectKeuzeActivity.java
+++ app/src/main/java/com/solutions/guidedrecovery/ipmedt4/TrajectKeuzeActivity.java
@@ -2,12 +2,17 @@ package com.solutions.guidedrecovery.ipmedt4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
-public class TrajectKeuze extends AppCompatActivity {
+public class TrajectKeuzeActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_traject_keuze);
+
+
     }
 }
